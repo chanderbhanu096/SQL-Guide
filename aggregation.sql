070a81784abad888fd71d8f2941dcb08da011479
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-20 21:57:37 +0100

    sum and count use

--- aggregation.sql
+++ aggregation.sql
@@ -11,19 +11,55 @@
   FROM accounts
   WHERE primary_poc IS NOT NULL
 
--- COUNT - COUNTS THE NUMBER OF ROWS IN A TABLE
-  SELECT COUNT(*)
-  FROM accounts
-  -- COUNTING creating a new column with the count
-  SELECT COUNT(primary_poc) AS orders_count
-  FROM accounts
-  -- COUNTING DISTINCT VALUES
-  SELECT COUNT(DISTINCT primary_poc)
-  FROM accounts
+"COUNT" use example   
+   
+    -- COUNT - COUNTS THE NUMBER OF ROWS IN A TABLE
+      SELECT COUNT(*)
+      FROM accounts
+    -- count return the count of rows with non-null values in the specified column.
+      SELECT COUNT(primary_poc) AS account_primary_poc_count
+      FROM accounts 
 
--- GROUPS THE DATA BY A SPECIFIC COLUMN and COUNTS THE NUMBER OF ROWS IN EACH GROUP
-  SELECT COUNT(primary_poc) AS orders_count,primary_poc
-  FROM accounts
-  GROUP BY primary_poc
+    -- COUNTING creating a new column with the count
+      SELECT COUNT(primary_poc) AS orders_count
+      FROM accounts
+    -- COUNTING DISTINCT VALUES
+      SELECT COUNT(DISTINCT primary_poc)
+      FROM accounts
 
--- 
+    -- GROUPS THE DATA BY A SPECIFIC COLUMN and COUNTS THE NUMBER OF ROWS IN EACH GROUP
+      SELECT COUNT(primary_poc) AS orders_count,primary_poc
+      FROM accounts
+      GROUP BY primary_poc
+
+"Sum" use example
+    -- can only be used with numeric columns
+    -- can only be applied to columns not like count which can be applied with * or a column name
+    -- SUM - ADDS UP THE VALUES IN A COLUMN
+      SELECT SUM(amount)
+      FROM orders
+    -- SUMMING A SPECIFIC COLUMN
+      SELECT SUM(amount) AS total_amount
+      FROM orders
+    -- SUMMING A SPECIFIC COLUMN AND GROUPING THE DATA BY ANOTHER COLUMN
+      SELECT SUM(amount) AS total_amount,primary_poc
+      FROM orders
+      GROUP BY primary_poc
+  
+  -- QUERIES ON SUM USE 
+  --Find the total amount of poster_qty paper ordered in the orders table.
+    SELECT SUM(poster_qty) as total_amount_posterqty
+    FROM ORDERS
+    723646
+
+  --Find the total amount of standard_qty paper ordered in the orders table.
+    SELECT SUM(standard_qty) as total_amount_standard_qty
+    FROM ORDERS
+    1938346
+
+  --Find the total dollar amount of sales using the total_amt_usd in the orders table.
+    SELECT SUM(total_amt_usd) as total_sales_total_amt_usd
+    FROM ORDERS
+    23141511.83
+  
+  
