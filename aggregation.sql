25e98ee841390333c6348ab9dea7c91c79de8f90
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-30 22:25:29 +0200

    queries having

--- aggregation.sql
+++ aggregation.sql
@@ -285,3 +285,41 @@
     GROUP BY a.id, a.name
     HAVING COUNT(*) > 20
     ORDER BY num_orders;
+      
+    --Which account has spent the least with us?
+      SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+      FROM accounts a
+      JOIN orders o
+      ON a.id = o.account_id
+      GROUP BY a.id, a.name
+      ORDER BY total_spent
+      LIMIT 1;
+
+    --Which accounts used facebook as a channel to contact customers more than 6 times?
+      SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+      FROM accounts a
+      JOIN web_events w
+      ON a.id = w.account_id
+      GROUP BY a.id, a.name, w.channel
+      HAVING COUNT(*) > 6 AND w.channel = 'facebook'
+      ORDER BY use_of_channel;
+
+    --Which account used facebook most as a channel?
+      SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+      FROM accounts a
+      JOIN web_events w
+      ON a.id = w.account_id
+      WHERE w.channel = 'facebook'
+      GROUP BY a.id, a.name, w.channel
+      ORDER BY use_of_channel DESC
+      LIMIT 1;
+    --This query above only works if there are no ties for the account that used facebook the most. It is a best practice to use a larger limit number first such as 3 or 5 to see if there are ties before using LIMIT 1.
+
+  --Which channel was most frequently used by most accounts?
+    SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+    FROM accounts a
+    JOIN web_events w
+    ON a.id = w.account_id
+    GROUP BY a.id, a.name, w.channel
+    ORDER BY use_of_channel DESC
+    LIMIT 10;
