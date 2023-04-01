a6c9bd1d14f16b82eb75588f6750b11502924808
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-04-01 18:48:40 +0200

    ...time

--- aggregation.sql
+++ aggregation.sql
@@ -346,4 +346,19 @@
     FROM orders
     GROUP BY 1
     oRDER BY 2
+  
+  -- Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?
+    SELECT DATE_PART('year', occurred_at) ord_year,  SUM(total_amt_usd) total_spent
+    FROM orders
+    GROUP BY 1
+    ORDER BY 2 DESC;
+  
+  --Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?
+    SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_spent
+    FROM orders
+    WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+    GROUP BY 1
+    ORDER BY 2 DESC; 
+
+
    
